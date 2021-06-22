20affd5f5801dcd47e8ad2589d6169d193c8cfc1
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-06-22 19:56:30 +0100

    Try building with clang-cl

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,12 +4,15 @@
 :: (set as env var so it's used by both meson and during build with g-ir-scanner)
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 
+:: ensure gir files are found
 set "XDG_DATA_DIRS=%XDG_DATA_DIRS%;%LIBRARY_PREFIX%\share"
 
-:: get mixed path (forward slash) form of prefix so host prefix replacement works
-set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
+:: force use of clang-cl
+:: set CXX as well, even though there is no C++ code, as this is necessary for g-ir-scanner to switch to using clang-cl as well...
+set "CC=clang-cl"
+set "CXX=clang-cl"
 
-%BUILD_PREFIX%\Scripts\meson.exe setup builddir --wrap-mode=nofallback --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dwith_introspection=true -Dwith_vapi=false
+%BUILD_PREFIX%\Scripts\meson.exe setup builddir --wrap-mode=nofallback --buildtype=release --prefix=%LIBRARY_PREFIX% --backend=ninja -Dwith_introspection=true -Dwith_vapi=false -Denable_tests=false
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
